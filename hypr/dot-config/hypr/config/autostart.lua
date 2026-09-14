239d6709119e83edd3c6c2dd17243a9a1802fba2
Adjective10111 <mm.80xefatzadeh@gmail.com>
2026-09-14 11:03:39 +0330

    added dark-mode settings

--- hypr/dot-config/hypr/config/autostart.lua
+++ hypr/dot-config/hypr/config/autostart.lua
@@ -1,8 +1,14 @@
 -- Auto-start config
 -- if you dont use UWSM add your auto start programs here, otherwise use XDG autostart https://wiki.archlinux.org/title/XDG_Autostart
 
+local gtk_theme = "gsettings set org.gnome.desktop.interface "
+local gtk4_theme = gtk_theme .. "color-scheme 'prefer-dark'"
+local gtk3_theme = gtk_theme .. "gtk-theme 'adw-gtk3-dark'"
+
 hl.on("hyprland.start", function ()
     hl.exec_cmd("dbus-update-activation-environment --systemd --all")
     hl.exec_cmd("noctalia")
     hl.exec_cmd("xhost +SI:localuser:root")
+    hl.exec_cmd(gtk3_theme)
+    hl.exec_cmd(gtk4_theme)
 end)
